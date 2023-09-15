e223c535ef0360bb9d255dd2fa664ec867a8e36f
Tom Davies <tom.davies@mcbk-0129.lan>
2023-09-15 18:57:48 +0100

    added frontend and finished backend

---
+++ frontend/src/App.js
@@ -0,0 +1,24 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Dashboard from "./pages/dashboard";
+import Login from "./pages/login";
+import Header from "./components/header";
+import Register from "./pages/register";
+
+function App() {
+  return (
+    <>
+      <Router>
+        <div className="container">
+          <Header />
+          <Routes>
+            <Route path="/" element={<Dashboard />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+          </Routes>
+        </div>
+      </Router>
+    </>
+  );
+}
+
+export default App;
