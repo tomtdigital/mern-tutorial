eb1243759d44634d7a0b1b3424038d328addd666
Tom Davies <tomdavies@McBk-0148.QLOCOM>
2024-01-03 17:29:54 +0000

    backend config updated for heroku

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+const path = require("path");
 const express = require("express");
 const dotenv = require("dotenv").config();
 const { errorHandler } = require("./middleware/error-middleware");
@@ -13,6 +14,17 @@ app.use(express.urlencoded({ extended: false }));
 // Routing
 app.use("/api/goals", require("./routes/goal-routes"));
 app.use("/api/users", require("./routes/user-routes"));
+// Serve frontend
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/build")));
+  app.get("*", (req, res) =>
+    res.sendFile(
+      path.resolve(__dirname, "../", "frontend", "build", "index.html")
+    )
+  );
+} else {
+  app.get("/", (req, res) => res.send("Please set to production"));
+}
 // Middleware - Error Handling
 app.use(errorHandler);
 // Receive incoming requests on a port
